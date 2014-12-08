fa6b481c09dafdef83f5f45182b67ba9fcb5f159
changzhi.wu <changzhi.wu@ck-telecom.com>
2014-12-08 19:12:12 +0800

    2014-12-08

--- TrainTicket/gen/com/ckt/trainticket/R.java
+++ TrainTicket/gen/com/ckt/trainticket/R.java
@@ -36,6 +36,7 @@ public final class R {
         public static final int has_more=0x7f090004;
         public static final int my12306=0x7f090003;
         public static final int my_order=0x7f090002;
+        public static final int quit=0x7f090006;
         public static final int serach_train=0x7f090001;
         public static final int viewpager=0x7f090000;
     }
@@ -49,11 +50,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
-        public static final int has_more=0x7f060006;
+        public static final int has_more=0x7f060007;
         public static final int hello_world=0x7f060001;
-        public static final int my12306=0x7f060005;
-        public static final int my_order=0x7f060004;
-        public static final int serach_train=0x7f060003;
+        public static final int my12306=0x7f060006;
+        public static final int my_order=0x7f060005;
+        public static final int quit=0x7f060003;
+        public static final int serach_train=0x7f060004;
     }
     public static final class style {
         /** 
