948b840b6530053f91423a034996be77be1acfc2
Francis <wuchangzhity@163.com>
2015-08-14 01:02:04 +0800

    NavigationMap

--- NavigationMap/app/src/main/java/com/ckt/francis/navigationmap/activitys/MainActivity.java
+++ NavigationMap/app/src/main/java/com/ckt/francis/navigationmap/activitys/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ckt.francis.navigationmap.activitys;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -16,7 +20,13 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.navisdk.adapter.BNRoutePlanNode;
+import com.baidu.navisdk.adapter.BaiduNaviManager;
 import com.ckt.francis.navigationmap.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.jpush.android.api.JPushInterface;
 
@@ -26,16 +36,20 @@ public class MainActivity extends Activity {
     private BaiduMap mBaiduMap;
     private LocationClient mLocClient;
     private boolean isFirstLoc = true;
-    public MyLocationListenner myListener = new MyLocationListenner();
+    private MyLocationListenner myListener = new MyLocationListenner();
+
+    private static final String APP_FOLDER_NAME = "BNSDKDemo";
+    public static final String ROUTE_PLAN_NODE = "routePlanNode";
+    private String authinfo = null;
+    private String mSDCardPath = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mMapView = (MapView) findViewById(R.id.bmapView);
-
         mBaiduMap = mMapView.getMap();
-
         mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
                 MyLocationConfiguration.LocationMode.NORMAL, true, null));
 
@@ -49,6 +63,11 @@ public class MainActivity extends Activity {
         mLocClient.setLocOption(option);
         mLocClient.start();
 
+
+        if ( initDirs() ) {
+           initNavi();
+        }
+        initListener();
     }
 
     @Override
@@ -68,6 +87,120 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    private boolean initDirs() {
+        mSDCardPath = getSdcardDir();
+        if ( mSDCardPath == null ) {
+            return false;
+        }
+        File f = new File(mSDCardPath, APP_FOLDER_NAME);
+        if ( !f.exists() ) {
+            try {
+                f.mkdir();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initNavi() {
+        BaiduNaviManager.getInstance().setNativeLibraryPath(mSDCardPath + "/BaiduNaviSDK_SO");
+        BaiduNaviManager.getInstance().init(this, mSDCardPath, APP_FOLDER_NAME,
+                new BaiduNaviManager.NaviInitListener() {
+                    @Override
+                    public void onAuthResult(int status, String msg) {
+                        if (0 == status) {
+                            authinfo = "key校验成功!";
+                        } else {
+                            authinfo = "key校验失败, " + msg;
+                        }
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, authinfo, Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                    }
+
+                    public void initSuccess() {
+                        Toast.makeText(MainActivity.this, "百度导航引擎初始化成功", Toast.LENGTH_SHORT).show();
+                    }
+
+                    public void initStart() {
+                        Toast.makeText(MainActivity.this, "百度导航引擎初始化开始", Toast.LENGTH_SHORT).show();
+                    }
+
+                    public void initFailed() {
+                        Toast.makeText(MainActivity.this, "百度导航引擎初始化失败", Toast.LENGTH_SHORT).show();
+                    }
+                }, null /*mTTSCallback*/);
+    }
+
+    private String getSdcardDir() {
+        if (Environment.getExternalStorageState().equalsIgnoreCase(
+                Environment.MEDIA_MOUNTED)) {
+            return Environment.getExternalStorageDirectory().toString();
+        }
+        return null;
+    }
+
+    private void initListener() {
+        LogUtil.d("------------1");
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if ( BaiduNaviManager.isNaviInited() ) {
+                    routeplanToNavi(BNRoutePlanNode.CoordinateType.WGS84);
+                }
+            }
+        });
+   //     if ( BaiduNaviManager.isNaviInited() ) {
+
+  //          routeplanToNavi(BNRoutePlanNode.CoordinateType.WGS84);
+//            routeplanToNavi(BNRoutePlanNode.CoordinateType.GCJ02);
+//            routeplanToNavi(BNRoutePlanNode.CoordinateType.BD09_MC);
+  //     }
+    }
+
+    private void routeplanToNavi(BNRoutePlanNode.CoordinateType coType) {
+        BNRoutePlanNode sNode = null;
+        BNRoutePlanNode eNode = null;
+        switch(coType) {
+            case GCJ02: {
+                sNode = new BNRoutePlanNode(116.30142, 40.05087,
+                        "百度大厦", null, coType);
+                eNode = new BNRoutePlanNode(116.39750, 39.90882,
+                        "北京天安门", null, coType);
+                break;
+            }
+            case WGS84: {
+                sNode = new BNRoutePlanNode(116.300821,40.050969,
+                        "百度大厦", null, coType);
+                eNode = new BNRoutePlanNode(116.397491,39.908749,
+                        "北京天安门", null, coType);
+                break;
+            }
+            case BD09_MC: {
+                sNode = new BNRoutePlanNode(12947471,4846474,
+                        "百度大厦", null, coType);
+                eNode = new BNRoutePlanNode(12958160,4825947,
+                        "北京天安门", null, coType);
+                break;
+            }
+            default : break;
+        }
+        if (sNode != null && eNode != null) {
+            List<BNRoutePlanNode> list = new ArrayList<BNRoutePlanNode>();
+            list.add(sNode);
+            list.add(eNode);
+            BaiduNaviManager.getInstance().launchNavigator(this, list, 1, true, new DemoRoutePlanListener(sNode));
+        }
     }
 
     @Override
@@ -115,6 +248,28 @@ public class MainActivity extends Activity {
         }
 
         public void onReceivePoi(BDLocation poiLocation) {
+        }
+    }
+
+    public class DemoRoutePlanListener implements BaiduNaviManager.RoutePlanListener {
+
+        private BNRoutePlanNode mBNRoutePlanNode = null;
+        public DemoRoutePlanListener(BNRoutePlanNode node){
+            mBNRoutePlanNode = node;
+        }
+
+        @Override
+        public void onJumpToNavigator() {
+            Intent intent = new Intent(MainActivity.this, BNDemoGuideActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(ROUTE_PLAN_NODE, (BNRoutePlanNode) mBNRoutePlanNode);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
+        @Override
+        public void onRoutePlanFailed() {
+            // TODO Auto-generated method stub
+
         }
     }
 }
